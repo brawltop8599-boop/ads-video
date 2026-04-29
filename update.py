8bb71592ee8954c26a28acc75caa4675ea6e4212
brawltop8599-boop <brawltop8599@gmail.com>
2026-04-29 10:53:34 +0300

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 # МЫ СМЕНИЛИ КЛИЕНТА НА VLC, ЧТОБЫ ПОЛУЧИТЬ СПИСОК, А НЕ МЕНЮ
-SOURCE_URL = "https://iptv-online.cc/my/97479/C8A918F9E52D9B8/m3u8"
+SOURCE_URL = "https://iptv-online.cc/my/97479/E740CF16EFAA743/m3u8"
 FILENAME = "my_list.m3u" 
 
 def run():
