e820849e236272910bca110fd2956fde7c168c03
brawltop8599-boop <brawltop8599@gmail.com>
2026-04-29 15:35:52 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import requests
 import re
 
-SOURCE_URL = "https://iptv-online.cc/my/97479/A41463A4526E745/m3u8" 
+SOURCE_URL = "https://iptv-online.cc/my/97479/51E6808E03E5E0D/m3u8" 
 FILENAME = "local_channels.txt"
 
 def run():
